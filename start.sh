f7d718e7b1e1a2b3df91cda91fed5eb6c5e55de6
e5renewal <69100083+e5renewal@users.noreply.github.com>
2022-04-13 12:33:40 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@
 # configs
 AUUID=7ade159c-8b3e-42be-b437-e2eb8f63f2ec
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/e5renewal/rail/main/etc/Caddyfile
-CONFIGXRUI=https://raw.githubusercontent.com/e5renewal/rail/main/etc/xrui.json
+CONFIGCADDY=https://raw.githubusercontent.com/e5renewal/rail2/main/etc/Caddyfile
+CONFIGXRUI=https://raw.githubusercontent.com/e5renewal/rail2/main/etc/xrui.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/e5renewal/rail/main/etc/StoreFiles
+StoreFiles=https://raw.githubusercontent.com/e5renewal/rail2/main/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
