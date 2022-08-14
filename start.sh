0e5c757b9a4879fbc839147cb1242f6cba174436
slcck <101538261+slcck@users.noreply.github.com>
2022-08-14 23:42:20 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@
 # configs
 AUUID=7ade159c-8b3e-42be-b437-e2eb8f63f2ec
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/dinaau/rail1/main/etc/Caddyfile
-CONFIGXRUI=https://raw.githubusercontent.com/dinaau/rail1/main/etc/xrui.json
+CONFIGCADDY=https://raw.githubusercontent.com/slcck/rail11/main/etc/Caddyfile
+CONFIGXRUI=https://raw.githubusercontent.com/slcck/rail11/main/etc/xrui.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/dinaau/rail1/main/etc/StoreFiles
+StoreFiles=https://raw.githubusercontent.com/slcck/rail11/main/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
